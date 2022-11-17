e9f9e9d2aa8d854831dbc9e533a695a5951c0b04
john mckenna <sndwrksdesign@gmail.com>
2022-11-16 18:43:18 -0700

    add head to see if it fixes some problems here

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,3 +43,19 @@ export default function Home () {
     </GlobalWrapper>
   );
 }
+
+export function Head () {
+  return (
+    <>
+      <title>John McKenna - Portfolio</title>
+      <meta charset="utf-8" />
+      <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="theme-color" content="#000000" />
+      <meta
+        name="description"
+        content="Web site created using create-react-app"
+      />
+    </>
+  );
+}
